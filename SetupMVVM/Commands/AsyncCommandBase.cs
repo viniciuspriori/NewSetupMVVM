dd50c7e5ce84b6ac7717444bc21832b8f8f74fcf
vinicius <priori987@gmail.com>
2023-02-19 19:49:01 -0300

    imported as a single dll, the only dependency is modal

---
+++ SetupMVVM/Commands/AsyncCommandBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMEssentials.Commands
+{
+    public abstract class AsyncCommandBase : CommandBase
+    {
+        private readonly Action<Exception> _onException;
+
+        private bool _isExecuting;
+        public bool IsExecuting
+        {
+            get
+            {
+                return _isExecuting;
+            }
+            private set
+            {
+                _isExecuting = value;
+                OnCanExecuteChanged();
+            }
+        }
+
+        public AsyncCommandBase(Action<Exception> onException = null)
+        {
+            _onException = onException;
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return !IsExecuting && base.CanExecute(parameter);
+        }
+
+        public override async void Execute(object parameter)
+        {
+            IsExecuting = true;
+
+            try
+            {
+                await ExecuteAsync(parameter);
+            }
+            catch (Exception ex)
+            {
+                _onException?.Invoke(ex);
+            }
+
+            IsExecuting = false;
+        }
+
+        protected abstract Task ExecuteAsync(object parameter);
+    }
+}
---
+++ SetupMVVM/Stores/ModalNavigationStore.cs
@@ -0,0 +1,34 @@
+﻿using MVVMEssentials.ViewModels;
+using System;
+
+namespace MVVMEssentials.Stores
+{
+    public class ModalNavigationStore : INavigationStore
+    {
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel?.Dispose();
+                _currentViewModel = value;
+                OnCurrentViewModelChanged();
+            }
+        }
+
+        public bool IsOpen => CurrentViewModel != null;
+
+        public event Action CurrentViewModelChanged;
+
+        public void Close()
+        {
+            CurrentViewModel = null;
+        }
+
+        private void OnCurrentViewModelChanged()
+        {
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+}
